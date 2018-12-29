792c2ea1da676c9c61e312208514f28cf454266f
hututu <853652726@qq.com>
2018-12-29 15:04:38 +0800

    test

---
+++ helloMaven/src/main/java/com/byd/web/addUser.java
@@ -0,0 +1,19 @@
+package com.byd.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class addUser extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name=request.getParameter("username");
+        System.out.println(name);
+    }
+}
